0c45291576ca48be9254eba42b18463bbc4f51f7
Ankitk25 <ankit707.25@gmail.com>
2025-06-16 09:03:08 +0530

    Control Flows

---
+++ JavaArrays/Level1/Vote.java
@@ -0,0 +1,17 @@
+package JavaArrays.Level1;
+import java.util.*;
+
+public class Vote {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int[] arr=new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=s.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>=18){
+                System.out.printf("The student with the age %d can vote",arr[i]);
+            }
+        }
+    }
+}
--- ProgrammingElements/Level1/BasicCalculator.java
+++ ProgrammingElements/Level1/BasicCalculator.java
@@ -1,3 +1,4 @@
+package ProgrammingEelments.Level1
 import java.util.Scanner;
 public class BasicCalculator {
 
